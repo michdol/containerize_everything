43996b728cebd22015fe9d73f9c39e32d0aa5b22
michdol <michal.dolbniak@gmail.com>
2020-06-19 17:57:35 +0900

    Annotated server.py

--- containers/server.py
+++ containers/server.py
@@ -2,7 +2,7 @@ import logging
 import select
 import socket
 
-from typing import Dict
+from typing import Dict, List
 
 import settings
 from custom_types.custom_types import Address
@@ -20,7 +20,7 @@ class Client(object):
     self.username: str = ''
 
   def __str__(self) -> str:
-    return "{}:{}".format(*self.address)
+    return "Client({}:{})".format(*self.address)
 
   def set_header(self, header):
     self.raw_header = header
@@ -42,16 +42,15 @@ class UndefinedClient(Client):
 
 class Server(object):
   def __init__(self, host: str, port: int):
-    self.host = host
-    self.port = port
+    self.address: Address = (host, port)
     self.clients: Dict = {}
     self.master: Client = UndefinedClient("", -1)
     self.server_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.server_socket.bind((self.host, self.port))
-    self.sockets = [self.server_socket]
+    self.server_socket.bind(self.address)
+    self.sockets: List[socket.socket] = [self.server_socket]
 
   def serve_forever(self):
-    logging.info("Listening on {}:{}".format(self.host, self.port))
+    logging.info("Listening on {}:{}".format(*self.address))
     self.server_socket.listen()
     while True:
       read, _, exception = select.select(self.sockets, [], self.sockets)
@@ -62,59 +61,64 @@ class Server(object):
         if notified_socket == self.server_socket:
           client_socket, client_address = self.server_socket.accept()
           client = Client(client_socket, client_address)
-          payload = self.receive_message(client)
-          if not payload:
-            logging.info("{}: Payload empty, connection closed".format(client))
+          incomming_payload: Dict = self.receive_message(client)
+          if not incomming_payload:
+            logging.info("{} Payload empty, connection closed".format(client))
             continue
-          client.set_header(payload["header"])
-          client.set_username(payload["data"])
+          client.set_header(incomming_payload["header"])
+          client.set_username(incomming_payload["data"])
           if client.is_master():
             logging.debug("Master connected")
             self.master = client
           self.sockets.append(client.socket)
           self.clients[client.socket] = client
-          logging.info("{}: New connection accepted".format(client))
+          logging.info("{} New connection accepted".format(client))
         else:
-          client = self.clients[notified_socket]
-          payload = self.receive_message(client)
-          if not payload:
-            logging.info("{}: Connection closed, removing client".format(client))
+          client: Client = self.clients[notified_socket]
+          incomming_payload: Dict = self.receive_message(client)
+          if not incomming_payload:
+            logging.info("{} Connection closed, removing client".format(client))
             self.sockets.remove(notified_socket)
             del self.clients[notified_socket]
             continue
-          logging.info("{}: Notifying other clients by {}".format(client, client.username))
-          for client_socket in self.clients:
-            if client_socket != notified_socket:
-              tmp = "{}{}{}{}".format(
-                client.raw_header, client.raw_username,
-                payload["header"], payload["data"]
-              )
-              client_socket.send(tmp.encode("utf-8"))
+          logging.info("{} Notifying other clients by {}".format(client, client.username))
+          payload: bytes = self.get_broadcast_payload(client, incomming_payload)
+          self.broadcast(client, payload)
 
       for notified_socket in self.exception_sockets:
         logging.info("Removing sockets with exception\t{}:{}".format(*notified_socket.getpeername()))
         self.sockets.remove(notified_socket)
         del self.clients[notified_socket]
 
-  def receive_message(self, client):
+  def broadcast(self, client: Client, payload):
+    for client_socket in self.clients:
+      if client_socket != client.socket:
+        client_socket.send(payload)
+
+  def get_broadcast_payload(self, client: Client, payload) -> bytes:
+    payload_str = "{client_header!r}{client_username!r}{payload_header!r}{payload!r}".format(
+      client_header=client.raw_header,
+      client_username=client.raw_username,
+      payload_header=payload["header"],
+      payload=payload["data"]
+    )
+    return payload_str.encode("utf-8")
+
+  def receive_message(self, client: Client) -> Dict[str, bytes]:
     try:
       logging.info("{}: Receiving a message".format(client))
-      message_header = client.socket.recv(HEADER_LENGTH)
+      message_header: bytes = client.socket.recv(HEADER_LENGTH)
       if len(message_header) == 0:
         logging.info("{}: No data received, connection has been closed.".format(client))
-        return
-      message_header_str = self.decode_bytes(message_header)
-      message_length = int(message_header_str.strip())
+        return {}
+      message_length = int(message_header.decode("utf-8"))
       logging.info("Message length %d", message_length)
-      message = client.socket.recv(message_length)
-      message_str = self.decode_bytes(message)
-      logging.info("{}: Received message\n{}".format(client, message_str))
+      message: bytes = client.socket.recv(message_length)
+      logging.info("{}: Received message\n{}".format(client, message.decode("utf-8")))
       return {"header": message_header, "data": message}
     except Exception as e:
       logging.error("{}: Error receiving a message: {}".format(client, e))
-
-  def decode_bytes(self, payload):
-    return payload.decode("utf-8")
+      return {}
 
 
 if __name__ == "__main__":
